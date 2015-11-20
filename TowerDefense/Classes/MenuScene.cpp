a707fd215bab56c37a18fb58d67a1da13bb21276
admin <admin@admins-iMac-6.local>
2015-11-20 10:00:48 +0900

    42

--- TowerDefense/Classes/MenuScene.cpp
+++ TowerDefense/Classes/MenuScene.cpp
@@ -1,24 +1,32 @@
-#include "HelloWorldScene.h"
+//
+//  Menu.cpp
+//  TowerDefense
+//
+//  Created by admin on 11/20/15.
+//
+//
+
+#include "MenuScene.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+Scene* MenuScene::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
     // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
-
+    auto layer = MenuScene::create();
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool MenuScene::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -29,28 +37,28 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+                                           CC_CALLBACK_1(MenuScene::menuCloseCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
-
+    
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
-
+    
     /////////////////////////////
     // 3. add your codes below...
-
+    
     // add a label shows "Hello World"
     // create and initialize a label
     
@@ -59,16 +67,16 @@ bool HelloWorld::init()
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height - label->getContentSize().height));
-
+    
     // add the label as a child to this layer
     this->addChild(label, 1);
-
+    
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("HelloWorld.png");
-
+    
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
+    
     // add the sprite as a child to this layer
     this->addChild(sprite, 0);
     
@@ -76,11 +84,12 @@ bool HelloWorld::init()
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void MenuScene::menuCloseCallback(Ref* pSender)
 {
     Director::getInstance()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
 }
+
---
+++ TowerDefense/Classes/MenuScene.h
@@ -0,0 +1,28 @@
+//
+//  Menu.h
+//  TowerDefense
+//
+//  Created by admin on 11/20/15.
+//
+//
+
+#ifndef __TowerDefense__MenuScene__
+#define __TowerDefense__MenuScene__
+
+#include "cocos2d.h"
+
+class MenuScene : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+    
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(MenuScene);
+};
+
+#endif /* defined(__TowerDefense__MenuScene__) */
