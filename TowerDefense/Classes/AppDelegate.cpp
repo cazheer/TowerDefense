b560a5c58a75595884fec06e48db5ac6d6dfb397
admin <admin@admins-iMac-6.local>
2015-11-25 13:34:02 +0900

    42

--- TowerDefense/Classes/AppDelegate.cpp
+++ TowerDefense/Classes/AppDelegate.cpp
@@ -73,6 +73,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     }
 
     register_all_packages();
+    
+    FileUtils::getInstance()->addSearchPath("res");
 
     // create a scene. it's an autorelease object
     auto scene = MenuScene::createScene();
