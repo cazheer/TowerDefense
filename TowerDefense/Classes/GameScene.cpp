7a3de6e5d494f472a43d8d047687440d559d2548
stvince <destvincent@hotmail.fr>
2015-12-10 06:11:38 +0900

    drag and drop

--- TowerDefense/Classes/GameScene.cpp
+++ TowerDefense/Classes/GameScene.cpp
@@ -12,6 +12,21 @@
 
 USING_NS_CC;
 
+cocos2d::Vec2 GameScene::posScreenToWord(cocos2d::Vec2 screen)
+{
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	return Vec2(int(screen.x / visibleSize.width * CASE_X),
+		int(screen.y / visibleSize.height * CASE_Y));
+}
+
+cocos2d::Vec2 GameScene::posWordToScreen(cocos2d::Vec2 word)
+{
+	auto origin = Director::getInstance()->getVisibleOrigin();
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	return Vec2(origin.x + word.x * visibleSize.width / CASE_X + 0.5f * visibleSize.width / CASE_X,
+		origin.y + word.y * visibleSize.height / CASE_Y + 0.5f * visibleSize.height / CASE_Y);
+}
+
 Scene* GameScene::createScene()
 {
     // 'scene' is an autorelease object
@@ -69,13 +84,98 @@ bool GameScene::init()
 
     resume();
 
+	// add background sprite
+	auto background = Sprite::create(FILE_GAME_BACKGROUND);
+	background->setScale(visibleSize.width / background->getContentSize().width, visibleSize.height / background->getContentSize().height);
+	background->setPosition(Vec2(
+		origin.x + visibleSize.width / 2,
+		origin.y + visibleSize.height / 2));
+	this->addChild(background);
+
+	//add images
+	for (int i = 0; i < 2; i++)
+	{
+		std::string str = "allies/" + std::to_string(i + 1) + ".png";
+		_dad[i] = Sprite::create(str);
+		_dad[i]->setScale(0.1f,0.1f);
+		_dad[i]->setPosition(posWordToScreen(Vec2(1 + i, 5)));
+		this->addChild(_dad[i]);
+	}
+	
     ////////////////////////////////
     // 3. Initialize the game data
     AllyManager::getInstance()->Initialize(this);
     EnemyManager::getInstance()->Initialize(this);
 
+
+
+	//drag and drop
+	auto listener1 = EventListenerTouchOneByOne::create();
+	log("test mouse");
+	// trigger when you push down
+	listener1->onTouchBegan = [this](Touch* touch, Event* event) {
+		log("mouse pressed");
+		auto target = static_cast<Sprite*>(event->getCurrentTarget());
+
+		//Get the position of the current point relative to the button
+		Point locationInNode = target->convertToNodeSpace(touch->getLocation());
+
+		//Check the click area
+		for (int i = 0; i < 2; i++)
+		{
+			if (_dad[i]->getBoundingBox().containsPoint(locationInNode))
+			{
+				std::string str = "allies/" + std::to_string(i + 1) + ".png";
+				_selected = Sprite::create(str);
+				_selected->setScale(0.1f, 0.1f);
+				_selected->setPosition(posWordToScreen(Vec2(1 + i, 6)));
+				_selected->setOpacity(180);
+				_selected_id = i;
+				this->addChild(_selected);
+				break;
+			}
+		}
+		
+		return true;
+	};
+
+	listener1->onTouchMoved = [this](Touch* touch, Event* event) {
+		if (_selected != NULL)
+		{
+			auto target = static_cast<Sprite*>(event->getCurrentTarget());
+			Point locationInNode = target->convertToNodeSpace(touch->getLocation());
+			Vec2 mouse = posScreenToWord(Vec2(locationInNode.x, locationInNode.y));
+			_selected->setPosition(posWordToScreen(Vec2(mouse.x, mouse.y)));
+		}
+	};
+
+	listener1->onTouchEnded = [this](Touch* touch, Event* event) {
+		if (_selected != NULL)
+		{
+			Vec2 pos = posScreenToWord(_selected->getPosition());
+			if (pos.x > 0 && pos.x < 11 && pos.y >= 0 && pos.y < 5)
+				createAlly(_selected_id, pos);
+			this->removeChild(_selected);
+			_selected = NULL;
+		}
+	};
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, this);
+
     this->scheduleUpdate();
     return true;
+}
+
+void GameScene::createAlly(int id, cocos2d::Vec2 pos)
+{
+	GameScene::Units unit;
+	std::string str = "allies/" + std::to_string(id + 1) + ".png";
+	unit.sprite = Sprite::create(str);
+	unit.sprite->setScale(0.1f, 0.1f);
+	unit.sprite->setPosition(posWordToScreen(pos));
+	this->addChild(unit.sprite);
+	//_allies.push_back(unit);
+
+
 }
 
 void GameScene::update(float deltaTime)
--- TowerDefense/Classes/GameScene.h
+++ TowerDefense/Classes/GameScene.h
@@ -17,30 +17,45 @@
 /////////////////////
 // MY CLASS DEFINE //
 /////////////////////
+# define    FILE_GAME_BACKGROUND            "bg.png"
 # define    FILE_GAME_PAUSE_BACKGROUND            ""
 # define    FILE_GAME_PAUSE_RESUMEBUTTON_NORMAL   ""
 # define    FILE_GAME_PAUSE_RESUMEBUTTON_SELECTED ""
 # define    FILE_GAME_PAUSE_MENUBUTTON_NORMAL     ""
 # define    FILE_GAME_PAUSE_MENUBUTTON_SELECTED   ""
 
+#define		CASE_X 12
+#define		CASE_Y 6
 class GameScene : public cocos2d::Layer
 {
     bool pause;
     cocos2d::Menu* optionMenu;
-
+	struct Units {
+		cocos2d::Sprite *sprite;
+		float _speed;
+		float _attack_speed;
+		float _force;
+		float _hp;
+	};
 public:
     static cocos2d::Scene* createScene();
     
     virtual bool init() override;
     virtual void update(float deltaTime) override;
-
     // pause gestion
     void pauseGame();
     void resumeGame();
     void menuGame();
-    
-    // implement the "static create()" method manually
+	void createAlly(int id, cocos2d::Vec2 pos);
+	cocos2d::Vec2 posWordToScreen(cocos2d::Vec2 word);
+	cocos2d::Vec2 posScreenToWord(cocos2d::Vec2 screen);
     CREATE_FUNC(GameScene);
+private:
+	cocos2d::Sprite *_dad[2];
+	std::vector<Units> _allies;
+	std::vector<Units> _enemies;
+	cocos2d::Sprite *_selected;
+	int _selected_id;
 };
 
 #endif /* defined(__TowerDefense__GameScene__) */
